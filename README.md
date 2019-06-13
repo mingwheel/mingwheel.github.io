d6a02ab5fbe3fa3a63ce03323ed47a57bcbab0f8
Ming <dev.sohml@gmail.com>
2019-06-13 21:42:01 +0800

    Finish the JavaScript gallery

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is the full starting application (with image gallery) for the final chapter of [*Learn Enough JavaScript to Be Dangerous*](https://www.learnenough.com/javascript-tutorial) by Michael Hartl, based on the sample application developed in [*Learn Enough CSS & Layout to Be Dangerous*](https://www.learnenough.com/css-and-layout-tutorial) by Lee Donahoe and Michael Hartl.
 
-You can visit the website [**here**](https://mingwheel.github.io/sample_website_full)
+You can visit the website [**here**](https://mingwheel.github.io/sample_website_full).
--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,3 +1,27 @@
 function activateGallery() {
-  alert("Hello from gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+  let title = document.querySelector("#gallery-info > .title");
+  let description = document.querySelector("#gallery-info > .description");
+
+  let currentClass = "current";
+
+  thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked img as main img
+      let newImgSrc = this.dataset.largeVersion;
+      let newImgAlt = this.dataset.title;
+      
+      mainImage.setAttribute("src", newImgSrc);
+      mainImage.setAttribute("alt", newImgAlt);
+
+      // Toggle highlight on img selection
+      document.querySelector(".current").classList.remove(currentClass);
+      this.parentNode.classList.add(currentClass);
+      
+      // Update img info
+      title.textContent = this.dataset.title;
+      description.textContent = this.dataset.description;
+    });
+  });
 }
